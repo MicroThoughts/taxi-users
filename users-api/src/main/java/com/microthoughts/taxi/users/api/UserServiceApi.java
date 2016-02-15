c1b2ca35b7efc3f3dc1960e6335b92d15fd94d84
Ilya_Zinkovich <Ilya_Zinkovich@epam.com>
2016-02-15 10:45:20 +0300

    Added method declaration to the swagger annotations

--- users-api/src/main/java/com/microthoughts/taxi/users/api/UserServiceApi.java
+++ users-api/src/main/java/com/microthoughts/taxi/users/api/UserServiceApi.java
@@ -12,23 +12,23 @@ import java.util.List;
 @RestController
 public interface UserServiceApi
 {
-    @ApiOperation(value = "Create user")
+    @ApiOperation(value = "Create user", httpMethod = "POST")
     @RequestMapping(value = "/users", method = RequestMethod.POST)
     void createUser(@RequestBody User user);
 
-    @ApiOperation(value = "Get all users")
+    @ApiOperation(value = "Get all users", httpMethod = "GET")
     @RequestMapping("/users")
     public List<User> getUsers();
 
-    @ApiOperation(value = "Get user by id")
+    @ApiOperation(value = "Get user by id", httpMethod = "GET")
     @RequestMapping("/users/{id}")
     public User getUser(@PathVariable("id") Long id);
 
-    @ApiOperation(value = "Update user")
+    @ApiOperation(value = "Update user", httpMethod = "PUT")
     @RequestMapping(value = "/users/{id}", method = RequestMethod.PUT)
     public void updateUser(@PathVariable Long id, @RequestBody User user);
 
-    @ApiOperation(value = "Delete user")
+    @ApiOperation(value = "Delete user", httpMethod = "DELETE")
     @RequestMapping(value = "/users/{id}", method = RequestMethod.DELETE)
     public void deleteUser(@PathVariable Long id);
 }
